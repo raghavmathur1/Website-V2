7d92760c7674c374691934a971e351da47df23ec
Raghav Mathur <raghav3501@gmail.com>
2021-08-27 09:48:03 +0530

    Updates

---
+++ precache-manifest.60a7b80385b13b2256495b7fbb6452ad.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5218cb19cfa4bae6ae10d51a431c041",
+    "url": "/Website-V2/index.html"
+  },
+  {
+    "revision": "9cba871cdcec69a8bc42",
+    "url": "/Website-V2/static/css/main.13a73956.chunk.css"
+  },
+  {
+    "revision": "2d876ee6b48aa26ef9f1",
+    "url": "/Website-V2/static/js/2.366ec20e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Website-V2/static/js/2.366ec20e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2851066be56041ecf073",
+    "url": "/Website-V2/static/js/3.8463e9b8.chunk.js"
+  },
+  {
+    "revision": "43830d0dd76f7b50d7a3",
+    "url": "/Website-V2/static/js/4.95a488fc.chunk.js"
+  },
+  {
+    "revision": "9cba871cdcec69a8bc42",
+    "url": "/Website-V2/static/js/main.44f81c1e.chunk.js"
+  },
+  {
+    "revision": "0fcd81a222117c3d0928",
+    "url": "/Website-V2/static/js/runtime-main.1cfd271c.js"
+  },
+  {
+    "revision": "95d55d992d7f63799ad116dbd12a25cf",
+    "url": "/Website-V2/static/media/Cera Pro Black.95d55d99.otf"
+  },
+  {
+    "revision": "afe6902a46c9d424e9aca916f97562db",
+    "url": "/Website-V2/static/media/Cera Pro Medium.afe6902a.otf"
+  },
+  {
+    "revision": "0cd32798eae3ceee200dc0d38cc88936",
+    "url": "/Website-V2/static/media/Cera Pro Regular.0cd32798.otf"
+  },
+  {
+    "revision": "62d5558ab1affe8d65561d9240891991",
+    "url": "/Website-V2/static/media/Cera Stencil PRO Black.62d5558a.otf"
+  },
+  {
+    "revision": "c7210af65e19c14a370e86364f299184",
+    "url": "/Website-V2/static/media/Cera Stencil PRO Medium.c7210af6.otf"
+  },
+  {
+    "revision": "002488293bced3f703c935c51ebd572a",
+    "url": "/Website-V2/static/media/Cera Stencil PRO.00248829.otf"
+  },
+  {
+    "revision": "bd7469bf5c54e01ba5dd593279a9c3b0",
+    "url": "/Website-V2/static/media/back3.bd7469bf.svg"
+  },
+  {
+    "revision": "ddc968298828dd9dd59a0d2f5cb014d5",
+    "url": "/Website-V2/static/media/gam.ddc96829.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ad2442ae3f4212851c817dc981069c0b.js"
+  "/Website-V2/precache-manifest.60a7b80385b13b2256495b7fbb6452ad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Website-V2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
